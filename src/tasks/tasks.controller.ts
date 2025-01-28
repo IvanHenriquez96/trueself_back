55dcd0911d37e709e6fa23e2073294678b5557b6
Ivan Henriquez <ihenriquez@licitalab.cl>
2025-01-27 21:48:15 -0300

    Implement TasksController and TasksService with CRUD operations

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  //Get All Tasks
+  @Get()
+  async getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  async createTask(@Body() new_task) {
+    return this.tasksService.createTask(new_task);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+const tasks = [{ name: 'tarea1' }, { name: 'tarea2' }];
+
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  getAllTasks() {
+    return [{ name: 'tarea1' }, { name: 'tarea2' }];
+  }
+
+  createTask(new_task) {
+    return [...tasks, new_task];
+  }
+}
